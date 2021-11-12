ba82dc5a4532744e10663181a42d870970d69210
mistrypm <89138127+mistrypm@users.noreply.github.com>
2021-11-12 14:59:26 -0500

    commit1- title change
    Changing the title.

--- GUI Application of Covid-19/corona-tracker - Copy.py
+++ GUI Application of Covid-19/corona-tracker - Copy.py
@@ -9,24 +9,21 @@ import pandas as pd                        # pip install pandas
 # ===================================================== defining function that generate status =======================================
 def show_data():
     data = covid.Covid()
-    country_name = e1.get()
-    status = data.get_status_by_country_name(country_name)
-    active = status['active']
-    e2.insert(0,active)
-    death = status['deaths']
-    e3.insert(0, death)
-    confirm = status['confirmed']
-    e4.insert(0, confirm)
-    recover = status['recovered']
-    e5.insert(0, recover)
+    patient_name = e1.get()
+    status = data.get_status_by_patient_name(patient_name)
+    vaccinated = status['vaccinated']
+    e2.insert(0,list_of_doses_received)
+    partiallyvaccinated = status['partiallyvaccinated']
+    e3.insert(0, vaccine_name_with_dose_number)
+    notvaccinated = status['not vaccinated']
+    e4.insert(0, need_vaccine)
     print(status)
     # intialise data of lists.
     data = {'id': status['id'],
-            'Country': status['country'],
-            'Confirmed': status['confirmed'],
-            'Active': status['active'],
-            'Deaths': status['deaths'],
-            'Recovered': status['recovered'],
+            'patient_name': status['patient_name'],
+            'vaccinated': status['receviedbooster'],
+            'partiallyvaccinated': status['partiallyvaccinated'],
+            'notvaccinated': status['notvaccinated'],
             'Latitude': status['latitude'],
             'Longitude': status['longitude'],
             'Last_Updated': status['last_update']
@@ -40,12 +37,12 @@ def show_data():
     cadr = {
 
         key:status[key]
-        for key in status.keys() & {"confirmed","active","deaths","recovered"}
+        for key in status.keys() & {"received booster","partially vaccinated","not vaccinated"}
     }
     n = list(cadr.keys())
     v = list(cadr.values())
-    plt.title("Country")
-    plt.bar(range(len(cadr)),v,tick_label=n,label=('active'))
+    plt.title("patient name")
+    plt.bar(range(len(cadr)),v,tick_label=n,label=('recevied booster'))
     plt.xlabel('x-labels')
     plt.ylabel('data')
 
@@ -59,40 +56,51 @@ def show_data():
 
 # ================================================= Window Design =========================================================
 master = tk.Tk()
-master.title('Covid-19 Vaccine status ')
+master.title('Covid-19 vaccine status ')
 
-tk.Label(master,text="COVID-19 COUNTRY STATUS" ,padx=50).grid(row=0)
+tk.Label(master,text="COVID-19 VACCINE STATUS" ,padx=50).grid(row=0)
 
-tk.Label(master, text="Enter your Country name : -").grid(row=2)
+tk.Label(master, text="Enter patient's first name : -").grid(row=2)
+
+e1 = tk.Entry(master)
+e1.grid(row=2, column=3)
+
+tk.Label(master, text="Enter patient's last name : -").grid(row=3)
 
 e1 = tk.Entry(master)
 
+e1.grid(row=3, column=3)
 
-e1.grid(row=2, column=3)
+tk.Label(master, text="Enter patient's date of Birth : -").grid(row=4)
 
+e1 = tk.Entry(master)
+
+e1.grid(row=4, column=3)
+
+tk.Label(master, text="Enter patient's MRN : -").grid(row=5)
+
+e1 = tk.Entry(master)
+
+e1.grid(row=5, column=3)
 tk.Button(master,
-          text='Show', command=show_data).grid(row=5,
+          text='Show', command=show_data).grid(row=7,
                                                        column=3,
                                                        sticky=tk.W,
                                                        pady=4)
 
 
-tk.Label(master, text="Active Cases : -").grid(row=8)
+tk.Label(master, text="vaccinated : -").grid(row=10)
 
 e2 = tk.Entry(master)
-e2.grid(row=8, column=3)
+e2.grid(row=10, column=3)
 
-tk.Label(master, text="Death Cases : -").grid(row=9)
+tk.Label(master, text="partially vaccinated : -").grid(row=11)
 e3 = tk.Entry(master)
-e3.grid(row=9, column=3)
+e3.grid(row=11, column=3)
 
-tk.Label(master, text="Confirmed Cases : -").grid(row=10)
+tk.Label(master, text="not vaccinated : -").grid(row=12)
 e4 = tk.Entry(master)
-e4.grid(row=10, column=3)
-
-tk.Label(master, text="Recovered Cases : -").grid(row=11)
-e5 = tk.Entry(master)
-e5.grid(row=11, column=3)
+e4.grid(row=12, column=3)
 
 master.mainloop()
 
